c432da8ec8d2004abe701ce7ce9192239eac2439
Moses Palacios <mospalacios@gmail.com>
2020-04-13 18:10:07 -0400

    added udpate method to user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,22 +3,28 @@ const db = require("../models");
 module.exports = {
     findAll: function(req, res) {
         db.User
-        .find(req.query)
-        .sort({name: 1})
-        .then(dbUser => res.json(dbUser))
-        .catch(err => res.status(422).json(err))
+            .find(req.query)
+            .sort({name: 1})
+            .then(dbUser => res.json(dbUser))
+            .catch(err => res.status(422).json(err))
     },
     findbyID: function(req, res){
         db.User
-        .findById(req.params.id)
-        .then(dbUser => res.json(dbUser))
-        .catch(err => res.status(422).json(err))
+            .findById(req.params.id)
+            .then(dbUser => res.json(dbUser))
+            .catch(err => res.status(422).json(err))
     },
     create: function(req, res){
         db.User
-        .create(req.body)
-        .then(dbUser => res.json(dbUser))
-        .catch(err => res.status(422).json(err))
+            .create(req.body)
+            .then(dbUser => res.json(dbUser))
+            .catch(err => res.status(422).json(err))
+    },
+    update: function (req, res) {
+        db.User
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     },
     remove: function (req, res) {
         db.User
