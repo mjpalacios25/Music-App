1464c5f1c459ece1c17101db13e7dce52fbd0925
Moses Palacios <mospalacios@gmail.com>
2020-04-29 18:54:52 -0400

    brought in changes from mosesTest branch

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,4 +5,24 @@ export default {
         return axios.get("/api/users")
     },
 
+    getSingleUser: function(id){
+        return axios.get("/api/users/" + id) 
+    },
+
+    getPlaylists: function(id){
+        return axios.get("/api/users/playlists/" + id)
+    },
+
+    createPlaylist: function(id, playlist){
+        return axios.post("/api/users/" + id, playlist)
+    },
+
+    removePlaylist: function(id){
+        return axios.delete("/api/users/" + id)
+    },
+
+    updatePlaylist: function(id, song){
+        return axios.put("/api/users/playlists/" + id, song)
+    }
+
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,21 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/UserController");
+const playlistController = require("../../controllers/PlaylistController");
 
 router.route("/")
     .get(usersController.findAll)
-    .post(usersController.create)
+    
 
 
 router.route("/:id")
     .get(usersController.findbyID)
-    .delete(usersController.remove)
-    .put(usersController.update)
-
+    .post(playlistController.create)
+    .delete(playlistController.remove)
+    
+router.route("/playlists/:id")
+    .get(playlistController.findbyID)
+    .put(playlistController.update)
+    
+    
 
 module.exports = router;
