e76573f607d669b26b17c2f36cd6c1d1acb75f7e
Andres Glasener <52112240+Aglasener@users.noreply.github.com>
2020-04-30 17:37:28 -0400

    posting new user from register works now, so does login verification

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,11 +4,13 @@ export default {
     getUsers: function() {
         return axios.get("/api/users")
     },
-    registerUser: function() {
-        return axios.post("/api/users/")
+    registerUser: function(req) {
+        console.log(req);
+        return axios.post("/api/users/", req)
     },
-    loginUser: function () {
-        return axios.post("/api/users/login")
+    loginUser: function (req) {
+        console.log("API " + JSON.stringify(req))
+        return axios.post("/api/users/login", req)
     }
 
  
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,35 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/UserController");
+const passport = require("passport")
 
 router.route("/")
     .get(usersController.findAll)
-    .post(usersController.validate, usersController.create)
+    // .post(usersController.validate, usersController.create)
+
+router.post("/", function(req,res) {
+    console.log("at api/users file " + JSON.stringify( req.body ))
+    
+        usersController.create(req,res)
+    
+})
 
 router.route("/logout")
     .post(usersController.logout)
 
-router.route("/login")
-    .post(usersController.logIn)
+router.post("/login", function (req, res, next) {
+    console.log('routes/user.js, login, req.body: ');
+    console.log(req.body)
+    next()
+},
+passport.authenticate('local'),
+(req, res) => {
+    console.log('logged in', req.user);
+    var userInfo = {
+        username: req.user.username
+    };
+    res.send(userInfo);
+}
+)
 
 router.route("/profile")
     .get(usersController.loggedIn)
