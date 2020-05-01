b30f01729b312ad75c178efe3df20eddc2b151a6
Moses Palacios <mospalacios@gmail.com>
2020-04-30 21:04:39 -0400

    updated search component to bring in logged in users id

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import Playlists from '../components/UserPlaylists'
-=======
->>>>>>> 64ead66a29148bc74871be7e2bb76acedb1599c9
 
 import React, {Component} from 'react'
 import { Redirect } from 'react-router-dom'
@@ -11,51 +6,24 @@ import Waveform from "../components/Wavesurfer"
 class Profile extends Component {
     constructor(props) {
         super(props);
-<<<<<<< HEAD
-        this.state = {
-            username: props.updateUser.username,
-            _id: props.updateUser._id
-=======
         console.log(props.username)
         console.log(props._id)
         this.state = {
             username: props.username,
             _id: props._id
 
->>>>>>> 64ead66a29148bc74871be7e2bb76acedb1599c9
 
         };
     }
 
-<<<<<<< HEAD
-        };
-    }
-
-=======
     
->>>>>>> 64ead66a29148bc74871be7e2bb76acedb1599c9
     render() {
         if (!this.state.username) {
             return <Redirect to={{ pathname: "/login" }} />
         } else {
         console.log(this.state.username)
         
-<<<<<<< HEAD
-        return (
-            <div>
-            <h1>Welcome</h1>
-<form action="/logout" method="POST">
-    <button type="submit">Log Out</button>
-</form>
-           
-            <Playlists updateUser={this._id} />
- 
-  
-            </div>
-        )
-=======
        return(
->>>>>>> 64ead66a29148bc74871be7e2bb76acedb1599c9
 
 
         <div>
